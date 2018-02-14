7ad8cb5a4206208b451456ad8ae2a56973e8e804
dav <dav@pc5-001-l.cs.st-andrews.ac.uk>
2018-02-14 11:25:17 +0000

    rename files

--- timer.py
+++ timer.py
@@ -5,13 +5,13 @@ import re
 solutionPatern = re.compile("solution")
 paramFilePatern = re.compile(r'.*\.param$')
 timePatern = re.compile(r'(\d+\.\d+)')
-solutionLengthPatern = re.compile(r'letting steps be (\d+)')
+fileNamePatern = re.compile(r'.*Bombastic(\d+)\_(\d+).*')
 logFile = "compTimes.log"
 
 
 optFlags = ['-O' + str(x) for x in range(4)]
 
-sub.call("rm -f " + logFile + " && touch " + logFile + " && echo \"file,optLevel,foundSolution,solutionLength,compTime\" >> "+logFile,shell=True)
+sub.call("rm -f " + logFile + " && touch " + logFile + " && echo \"instance_id,steps,optLevel,foundSolution,compTime\" >> "+logFile,shell=True)
 
 
 for root, dirs, files in os.walk("."):
@@ -20,20 +20,24 @@ for root, dirs, files in os.walk("."):
 		if not paramFilePatern.match(filePath) or re.search(r'stac', filePath):
 			continue
 		else:
+			fileSearch = fileNamePatern.search(filePath)
+                        instance_id = fileSearch.group(1)
+                        steps  = fileSearch.group(2)
+
 			for flag in optFlags:
+				lineOut = str(instance_id) + "," + str(steps) + ","
 				print(file,flag)
 				subOut = sub.check_output("TIMEFORMAT='%U'; { time ../savilerow Bombastic.eprime "+filePath+" -run-solver "+flag+"; } 2>&1",shell=True)
-				lineOut = str(filePath)
-				lineOut += "," + flag[-1]
+				lineOut += flag[-1] + ","
 				if solutionPatern.search(subOut):
-					lineOut += ",true,"
+					lineOut += "true,"
 				else:
-					lineOut += ",false,"
+					lineOut += "false,"
 
-				solutionOutput = sub.check_output("cat " + filePath,shell=True)
-                                solutionSearch = solutionLengthPatern.search(solutionOutput)
-                                solutionLength = solutionSearch.group(1)
-                                lineOut += str(solutionLength) + ","
+				#solutionOutput = sub.check_output("cat " + filePath,shell=True)
+                                #solutionSearch = solutionLengthPatern.search(solutionOutput)
+                                #solutionLength = solutionSearch.group(1)
+                                #lineOut += str(solutionLength) + ",
 
 				match = timePatern.search(subOut)
 				lineOut += match.group(0)
